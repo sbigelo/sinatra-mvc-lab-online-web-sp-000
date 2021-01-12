63654c456858b0091e07f12c06a278a73f05576a
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-12 16:52:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/piglantinize' do
+  post '/piglatinize' do
     pl = PigLatinizer.new
     @piglatin = pl.piglatinize(params[:user_phrase])
     erb :results
