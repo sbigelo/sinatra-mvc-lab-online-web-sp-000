9c311f0dc4b5c2b7da425c2c407ae309bebc149f
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-12 16:35:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglantinize' do
+    @piglantinize = PigLatinizer.new(params[:piglantinize])
+    erb :user_input
+  end
+
 end
